1ddc8243f10146c254ceeab16fe5cd03ed206099
rockbenben <qingwhat@gmail.com>
2022-07-23 03:24:43 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,17 @@ export default defineUserConfig({
   title: "诺一发稿平台",
   description: "海量一手媒体资源的中文互联网宣发平台，为广告公司提供全网最直接的媒体发布价格",
 
-  base: "/info/news-release/",
+  base: "/info/news-release/",  
+
+  head: [
+    [
+      "meta",
+      {
+        name: "keywords",
+        content: "新闻发稿,品牌宣传,网络媒体,新媒体发稿",
+      },
+    ],
+  ],
 
   theme,
   plugins: [
